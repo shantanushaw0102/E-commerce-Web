60e9ba70058eba44f8d052e9cda887705c3aebdd
shantanu <shantanushaw0102@gmail.com>
2023-10-23 21:32:47 +0530

    new things added

--- e-com-web/src/Home.jsx
+++ e-com-web/src/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import HeroSection from "./components/HeroSection";
+import Trusted from "./components/Trusted";
+import Services from "./components/Services";
 
 const Home = () => {
-  return <Wrapper className="test">Home</Wrapper>;
+  const data = {
+    name: " Apple Store",
+    description:
+      "The Apple Store, a tech enthusiast's haven, is a sleek and modern retail space where cutting-edge technology and innovative design converge. Here, you'll find a curated selection of Apple's iconic products, from the latest iPhones and MacBooks to an array of accessories and software. The store's minimalist aesthetics and friendly, knowledgeable staff create an immersive and educational shopping experience, making it the go-to destination for anyone seeking the best in tech and style.",
+  };
+
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
-
-const Wrapper = styled.section`
-  height: 100vh;
-  background-color: ${({ theme }) => theme.colors.bg};
-`;
-
 export default Home;
