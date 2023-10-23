be4aca97d8b61a1962661107fcc429a17325a1c7
shantanu <shantanushaw0102@gmail.com>
2023-10-23 21:33:04 +0530

    new things added

--- e-com-web/src/components/Header.jsx
+++ e-com-web/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
 const MainHeader = styled.header`
   padding: 0 4.8rem;
   height: 10rem;
-  background-color: ${({ theme }) => theme.colors.bg};
+  background-color: ${({ theme }) => theme.colors.headerbg};
   display: flex;
   justify-content: space-between;
   align-items: center;
