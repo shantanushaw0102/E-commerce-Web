e55e0f1ca5b40f6dfd2f35cb2e2dc5a38be753d6
shantanu <shantanushaw0102@gmail.com>
2023-10-31 16:59:29 +0530

    new filter context added

--- e-com-web/src/context/FilterContext.jsx
+++ e-com-web/src/context/FilterContext.jsx
@@ -7,7 +7,11 @@ const FilterContext = createContext();
 const initialState = {
   filter_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
+  sorting_value: "lowest",
+  filters: {
+    text: "",
+  },
 };
 
 export const FilterContextProvider = ({ children }) => {
@@ -15,23 +19,53 @@ export const FilterContextProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // set grid view
 
-//set grid view
   const setGridView = () => {
     return dispatch({ type: "SET_GRID_VIEW" });
   };
 
+  // set list view
 
-  //set list view
   const setListView = () => {
     return dispatch({ type: "SET_LIST_VIEW" });
   };
+
+  // sorting function
+
+  const sorting = (event) => {
+    let userValue = event.target.value;
+    dispatch({ type: "GET_SORT_VALUE", payload: userValue });
+  };
+
+  // update the filter values
+
+  const updateFilterValue = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    return dispatch({ type: "UPDATE_FILTERS_VALUE", payload: { name, value } });
+  };
+
+  // to sort the products
+
+  useEffect(() => {
+    dispatch({type:"FILTER_PRODUCTS"})
+    dispatch({ type: "SORTING_PRODUCTS", payload: products });
+  }, [products,state.sorting_value,state.filters]);
 
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
   return (
-    <FilterContext.Provider value={{ ...state ,setGridView,setListView}}>
+    <FilterContext.Provider
+      value={{
+        ...state,
+        setGridView,
+        setListView,
+        sorting,
+        updateFilterValue,
+      }}
+    >
       {children}
     </FilterContext.Provider>
   );
