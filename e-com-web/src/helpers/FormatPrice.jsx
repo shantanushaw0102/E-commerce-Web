7b56843f79a42b9c088e2734207b076f23afc36f
shantanu <shantanushaw0102@gmail.com>
2023-11-04 22:45:40 +0530

    modified

--- e-com-web/src/helpers/FormatPrice.jsx
+++ e-com-web/src/helpers/FormatPrice.jsx
@@ -5,5 +5,12 @@ const FormatPrice = ({ price }) => {
     maximumFractionDigits: 2,
   }).format(price / 100);
 };
+// const FormatPriceCart = ({ price }) => {
+//   return Intl.NumberFormat("en-IN", {
+//     style: "currency",
+//     currency: "INR",
+//     maximumFractionDigits: 2,
+//   }).format(price / 10);
+// };
 
 export default FormatPrice;
